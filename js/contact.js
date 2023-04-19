54f6d9b77a386468f9aab0cad66522b087f9eb69
Tirth Vora <71544532+tirth014@users.noreply.github.com>
2023-04-19 18:45:35 -0400

    shows-current-link-nav

--- js/contact.js
+++ js/contact.js
@@ -90,10 +90,22 @@ function validate()
     }
 }
 
+function currentLink(){
+    document.querySelectorAll(".nav-link").forEach
+    (link => {
+        if(link.href === window.location.href)
+        {
+            link.setAttribute('aria-current', 'page')
+        }
+    })
+}
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
     $("#submit").addEventListener("click", validate);
     remainingWords();
+
+    currentLink();
     
 });

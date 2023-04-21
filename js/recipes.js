0d58a83b0763eb3b6b9145f422f4543bd45df016
RichaShukla16 <130796298+RichaShukla16@users.noreply.github.com>
2023-04-20 23:58:47 -0400

    search

--- js/recipes.js
+++ js/recipes.js
@@ -19,3 +19,37 @@ document.addEventListener("DOMContentLoaded", ()=>{
     currentLink();
     
 });
+// function myFunction() {
+//     var dots = document.getElementById("dots");
+//     var moreText = document.getElementById("more");
+//     var btnText = document.getElementById("myBtn");
+  
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Read more";
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Read less";
+//       moreText.style.display = "inline";
+//     }
+//   }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const readMoreBtn = document.querySelector('.read');
+  const moreText = document.querySelector('.more');
+
+  // add a click event listener to the button
+  readMoreBtn.addEventListener('click', function() {
+    // if the additional text is hidden, show it and change the button text to "Read less"
+    if (moreText.style.display === 'none') {
+      moreText.style.display = 'inline';
+      readMoreBtn.textContent = 'Read less';
+    } 
+    // if the additional text is showing, hide it and change the button text to "Read more"
+    else {
+      moreText.style.display = 'none';
+      readMoreBtn.textContent = 'Read more';
+    }
+  });
+});
